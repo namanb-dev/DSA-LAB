00b18fdd6812508b9d6c96c05a9e754a7e84a40f
Naman Bansal <naman.work19@gmail.com>
2025-09-16 13:13:58 +0530

    Create Q2.cpp

---
+++ Assignment_2/Q2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+void print(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+
+int arr[7] = {64, 34, 25, 12, 22, 11, 90};
+bubbleSort(arr, 7);
+print(arr, 7);
+
+
+
+}
