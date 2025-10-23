4f4c2aecc56d36a0ce9dff6e344235dbc5928b99
Naman Bansal <naman.work19@gmail.com>
2025-10-23 22:15:19 +0530

    Update Q2.cpp

--- Assignment_2/Q2.cpp
+++ Assignment_2/Q2.cpp
@@ -1,29 +1,39 @@
+// Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping
+// the adjacent elements if they are in wrong order. Code the Bubble sort with the
+// following elements:
+
 #include <iostream>
 using namespace std;
 
-void bubbleSort(int arr[], int size) {
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                swap(arr[j], arr[j + 1]);
+int Bubble_sort(int arr[], int size)
+{
+    int i, j;
+    for (i = 0; i < size; i++)
+    {
+        for (j = i + 1; j < size; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
     }
 }
 
-void print(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
+void print(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << "";
     }
-    cout << endl;
 }
+int main()
+{
+    int arr[7] = {64, 34, 25, 12, 22, 11, 90};
+    Bubble_sort(arr, 7);
+    print(arr, 7);
 
-int main(){
-
-int arr[7] = {64, 34, 25, 12, 22, 11, 90};
-bubbleSort(arr, 7);
-print(arr, 7);
-
-
-
+    return 0;
 }
